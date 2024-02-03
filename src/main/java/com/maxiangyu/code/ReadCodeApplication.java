de284b4da614fce3f87ca6af5b93cbec641b6015
maxiangyu <coderjing@163.com>
2024-02-03 11:38:34 +0800

    增加日志

--- src/main/java/com/maxiangyu/code/ReadCodeApplication.java
+++ src/main/java/com/maxiangyu/code/ReadCodeApplication.java
@@ -1,12 +1,17 @@
 package com.maxiangyu.code;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReadCodeApplication {
 
+	private final static Logger log = LoggerFactory.getLogger(ReadCodeApplication.class);
+
 	public static void main(String[] args) {
+		log.info("正在启动springboot应用...");
 		SpringApplication.run(ReadCodeApplication.class, args);
 	}
 
